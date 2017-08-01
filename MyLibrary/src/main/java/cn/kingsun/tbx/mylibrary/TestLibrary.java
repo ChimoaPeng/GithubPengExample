926a40bd859b0f7a3df7bd5166754d5c957733e7
Zhiming Peng <592613492@qq.com>
2017-08-01 14:23:22 +0800

    recode

---
+++ MyLibrary/src/main/java/cn/kingsun/tbx/mylibrary/TestLibrary.java
@@ -0,0 +1,13 @@
+package cn.kingsun.tbx.mylibrary;
+
+/**
+ * Created by Administrator on 2017/8/1.
+ */
+
+public class TestLibrary {
+
+    public String getString(String in) {
+        return "TestLibrary " + in;
+    }
+
+}
